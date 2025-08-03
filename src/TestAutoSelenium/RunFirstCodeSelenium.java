24b92d9c139abedd5934977340d013e96ebbbae6
linhdoan19 <100949171+linhdoan19@users.noreply.github.com>
2025-08-03 22:43:00 +0700

    Add first commit

---
+++ src/TestAutoSelenium/RunFirstCodeSelenium.java
@@ -0,0 +1,28 @@
+
+package TestAutoSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RunFirstCodeSelenium {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Dowload\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.navigate().to("https://anhtester.com");
+
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(2000);
+		// Thoát hẳn Browser
+
+		driver.quit();
+
+	}
+
+}
